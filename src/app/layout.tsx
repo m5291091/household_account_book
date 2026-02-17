c711b465c2bcef7ca7fdd19286bd9279160712e4
Kenjo Yamamoto <applem3ky@gmail.com>
2026-02-17 17:31:13 +0900

    layout.tsxにimportを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
-import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
